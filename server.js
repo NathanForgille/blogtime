b216623fdf13a62d05bb9255f865ba21727cf89e
Jean Ralphio <nateforgille@outlook.com>
2021-06-09 23:47:40 -0500

    some routes were mislabeled in the handlebars between saying logout verses sign out and vice versa

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const routes = require('./controller');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('express-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
